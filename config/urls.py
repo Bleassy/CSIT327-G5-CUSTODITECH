73fefd3f9db2fdb00bfdcb3e4e4971a196015190
theo2815 <theocedricchan28@gmail.com>
2025-11-02 15:59:19 +0800

    Add root URL redirect to login

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=True)),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboards.urls')),
