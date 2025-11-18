9728b169f934ce7d8a34b1045ea4d8ff555c0ab6
theo2815 <theocedricchan28@gmail.com>
2025-11-19 03:29:29 +0800

    Refactor: Cleanup code comments and finalize frontend logic for production

--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# This command just collects your static files
 python manage.py collectstatic --noinput
 
-# DO NOT run makemigrations or migrate here
